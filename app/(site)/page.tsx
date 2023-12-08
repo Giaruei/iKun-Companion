5ca16c717812f4683505321d23435577f4a9f785
giaruei <247658354@qq.com>
2023-12-08 15:55:29 +0800

    Fix some styles

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
-    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+    <div className="flex h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
